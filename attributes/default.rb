a0d0865871d372568692ff2aae1a3e1741a3136b
Martin Fenner <mfenner@plos.org>
2014-12-06 11:52:09 -0800

    install packages

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['ruby']['packages'] = %w{ curl git avahi-daemon libnss-mdns python-software-properties software-properties-common }
+default["dotenv"] = "default"
+default["application"] = "almability"
