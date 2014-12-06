8e4581a03f5248168597ebfd57bd918ac0ae6f87
Martin Fenner <mfenner@plos.org>
2014-12-06 14:53:23 -0800

    include nginx recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,10 @@
 # install and configure dependencies
 include_recipe "apt"
 include_recipe "ruby"
+
+# install nginx and create configuration file and application root
+passenger_nginx ENV['APPLICATION'] do
+  user            ENV['DEPLOY_USER']
+  group           ENV['DEPLOY_GROUP']
+  action          :config
+end
