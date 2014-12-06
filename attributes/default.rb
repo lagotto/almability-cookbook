efbe4e9e1368b589f5fecd115bb1ab0f38d9ae2d
Martin Fenner <mfenner@plos.org>
2014-12-06 12:16:24 -0800

    install development tools

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default['ruby']['packages'] = %w{ curl git avahi-daemon libnss-mdns python-software-properties software-properties-common }
+default['ruby']['packages'] = %w{ curl git build-essential avahi-daemon libnss-mdns python-software-properties software-properties-common }
 default["dotenv"] = "default"
 default["application"] = "almability"
