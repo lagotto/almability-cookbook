8e1a6b539e387f2f5f3bae7a40f884fc537ecc11
Martin Fenner <mfenner@plos.org>
2014-12-06 14:57:14 -0800

    load dotenv file

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Almability configuration"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.5"
+version           "0.1.6"
 depends           "apt"
 
 # our own cookbooks
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,13 @@
 include_recipe "apt"
 include_recipe "ruby"
 
+# load .env configuration file with ENV variables
+# copy configuration file to shared folder
+dotenv node["application"] do
+  dotenv          node["dotenv"]
+  action          :nothing
+end.run_action(:load)
+
 # install nginx and create configuration file and application root
 passenger_nginx ENV['APPLICATION'] do
   user            ENV['DEPLOY_USER']
